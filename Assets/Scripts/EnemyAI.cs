d4953992c26b79c5193011b624793c90876cd865
Claudio-Visentin <D16126698@mydit.ie>
2019-01-26 16:31:14 +0000

    Sounds test

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
+    public int currentstate;
+    public int limit;
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/Scripts/Footsteps.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour
+{
+    public CharacterController character;
+    public AudioSource source;
+    public AudioClip steps;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+     if (character.velocity != Vector3.zero)
+        {
+            print("tea");
+            source.Play();
+        }   
+     if (character.velocity == Vector3.zero)
+        {
+            source.Stop();
+        }
+    }
+}
