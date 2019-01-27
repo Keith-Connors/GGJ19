7f2b172572bb073043c1e0a95cc71268e269e744
Claudio-Visentin <D16126698@mydit.ie>
2019-01-27 13:24:15 +0000

    qwe

---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    [SerializeField]
+    Canvas m_SettingsCanvas;
+    [SerializeField]
+    Canvas m_AccessibilityCanvas;
+
+
+    [SerializeField]
+    bool isPaused;
+
+<<<<<<< HEAD
+
+
+=======
+    
+>>>>>>> 5b174ba23c7ca111ef6e7188a473cf6cd5d725ec
+
+    private void Start()
+    {
+        m_SettingsCanvas.enabled = false;
+        m_AccessibilityCanvas.enabled = false;
+        isPaused = false;
+    }
+
+    private void Update()
+    {
+        if (isPaused == true)
+        {
+            Time.timeScale = 0;
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            Time.timeScale = 1;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            openSettings();
+        }
+    }
+
+    public void openSettings()
+    {
+        isPaused = true;
+<<<<<<< HEAD
+        m_SettingsCanvas.enabled = true;
+        m_AccessibilityCanvas.enabled = false;
+        
+=======
+        m_SettingsCanvas.enabled =  true;
+>>>>>>> 5b174ba23c7ca111ef6e7188a473cf6cd5d725ec
+    }
+
+    public void closeSettings()
+    {
+        isPaused = false;
+        m_SettingsCanvas.enabled = false;
+<<<<<<< HEAD
+        m_AccessibilityCanvas.enabled = false;
+    }
+
+    public void openAccessibilityMenu()
+    {
+        isPaused = true;
+        m_SettingsCanvas.enabled = false;
+        m_AccessibilityCanvas.enabled = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void closeAccessibilityMenu()
+    {
+        m_AccessibilityCanvas.enabled = false;
+        m_SettingsCanvas.enabled = false;
+        isPaused = false;
+    }
+
+    public void loadNormalVision() //Turn Off Color Blind assistance
+    {
+        m_colorBlind.Type = 0;
+        m_SettingsCanvas.enabled = false;
+        m_AccessibilityCanvas.enabled = false;
+        isPaused = false;
+    }
+
+    public void loadProtanopiaVision() //Protanopia
+    {
+        m_colorBlind.Type = 1;
+        m_SettingsCanvas.enabled = false;
+        m_AccessibilityCanvas.enabled = false;
+        isPaused = false;
+    }
+
+    public void loadDeuteranopiaVision() //Deuteranopia
+    {
+        m_colorBlind.Type = 2;
+        m_SettingsCanvas.enabled = false;
+        m_AccessibilityCanvas.enabled = false;
+        isPaused = false;
+    }
+
+    public void loadTritanopiaVision() //Tritanopia
+    {
+        m_colorBlind.Type = 3;
+        m_SettingsCanvas.enabled = false;
+        m_AccessibilityCanvas.enabled = false;
+        isPaused = false;
+    }
+ 
+}
+=======
+    }
+}
+>>>>>>> 5b174ba23c7ca111ef6e7188a473cf6cd5d725ec
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public float timer = 0.0f;
+    public EventInteract[] events;
+    public int currentevent;
+    bool chosen = false;
+
+    private void Start()
+    {
+        
+    }
+    void Update()
+    {
+        timer += Time.deltaTime;
+        float seconds = timer % 60;
+        if (timer % 5 == 0)
+        {
+            currentevent = Random.Range(0, events.Length);
+            events[currentevent].isactive = true;
+        }
+    }
+<<<<<<< HEAD
+    IEnumerator choose ()
+    {
+        if (chosen == false);
+        currentevent = Random.Range(0, events.Length);
+        events[currentevent].isactive = true;
+        chosen = true;
+        yield return new WaitForSeconds(5);
+        
+    }
+=======
+>>>>>>> 5b174ba23c7ca111ef6e7188a473cf6cd5d725ec
+}
